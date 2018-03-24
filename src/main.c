67384e510cebf25d377fc4707685d8ed8e337808
musaprg <musaprg@users.noreply.github.com>
2018-03-24 16:01:06 +0900

    Run based on byte size that get via serial connection

--- src/main.c
+++ src/main.c
@@ -7,21 +7,36 @@
 
 typedef int (*functype)(void);
 
+void printf(char *str){
+	int i;
+	for(i=0;str[i]!='\0';i++){
+		while (!(MU_LSR & MU_LSR_TX_IDLE) && !(MU_LSR & MU_LSR_TX_EMPTY));
+		MU_IO = (unsigned int)str[i];
+	}
+}
+
 int main(void)
 {
 	volatile char data;
 	volatile unsigned char * p = (volatile unsigned char *)ENTRY_ADDRESS;
-	int i;
+	int i, psize = 0;
 
-	for(i=0;i<0x54;i++) {
+	// get program size
+	while (!(MU_LSR & MU_LSR_RX_RDY));
+	psize = (int)MU_IO;
+
+	if (psize == 0) { // if psize is yet zero, return "Error" message.
+		printf("Error");
+		while(1);	
+	}
+
+	for(i=0;i<psize;i++) {
 		while (!(MU_LSR & MU_LSR_RX_RDY));
 		data = (unsigned char)MU_IO;
 		*(p++) = (unsigned char)data; // write 1 byte to memory
 	}
 
-	while (!(MU_LSR & MU_LSR_TX_IDLE) && !(MU_LSR & MU_LSR_TX_EMPTY));
-	MU_IO = (unsigned int)'A';
-
+	printf("A");
 
 	functype f = (functype)ENTRY_ADDRESS;
 	f();
