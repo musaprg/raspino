71faad743c26798fbf82281944d37baca5cdc86d
musaprg <musaprg@users.noreply.github.com>
2018-03-24 15:33:17 +0900

    completed

--- src/main.c
+++ src/main.c
@@ -3,27 +3,25 @@
 #define MU_LSR_TX_IDLE	(1U << 6)
 #define MU_LSR_TX_EMPTY	(1U << 5)
 #define MU_LSR_RX_RDY	(1U << 0)
-#define ENTRY_ADDRESS	0x90000
+#define ENTRY_ADDRESS	0x100000
+
+typedef int (*functype)(void);
 
 int main(void)
 {
 	volatile char data;
 	volatile unsigned char * p = (volatile unsigned char *)ENTRY_ADDRESS;
+	int i;
 
-	while (1) {
+	for(i=0;i<0x54;i++) {
 		while (!(MU_LSR & MU_LSR_RX_RDY));
 		data = (unsigned char)MU_IO;
-
-		if (data == 0x1a) break; // 0x1a : EOF
-
 		*(p++) = (unsigned char)data; // write 1 byte to memory
-
 	}
 
-	//while (!(MU_LSR & MU_LSR_TX_IDLE) && !(MU_LSR & MU_LSR_TX_EMPTY));
-	//MU_IO = (unsigned int)'A';
+	while (!(MU_LSR & MU_LSR_TX_IDLE) && !(MU_LSR & MU_LSR_TX_EMPTY));
+	MU_IO = (unsigned int)'A';
 
-	typedef int (*functype)(void);
 
 	functype f = (functype)ENTRY_ADDRESS;
 	f();
