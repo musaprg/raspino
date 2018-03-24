52616b84d378c0155186fe64094a2f8d8918875f
musaprg <musaprg@users.noreply.github.com>
2018-03-24 12:29:13 +0900

    Implement recieving binary from PC and executing

--- src/main.c
+++ src/main.c
@@ -3,18 +3,30 @@
 #define MU_LSR_TX_IDLE	(1U << 6)
 #define MU_LSR_TX_EMPTY	(1U << 5)
 #define MU_LSR_RX_RDY	(1U << 0)
+#define ENTRY_ADDRESS	0x90000
 
 int main(void)
 {
-	volatile char ch;
+	volatile char data;
+	volatile unsigned char * p = (volatile unsigned char *)ENTRY_ADDRESS;
 
 	while (1) {
 		while (!(MU_LSR & MU_LSR_RX_RDY));
-		ch = (char)MU_IO;
+		data = (unsigned char)MU_IO;
 
-		while (!(MU_LSR & MU_LSR_TX_IDLE) && !(MU_LSR & MU_LSR_TX_EMPTY));
-		MU_IO = (unsigned int)ch;
+		if (data == 0x1a) break; // 0x1a : EOF
+
+		*(p++) = (unsigned char)data; // write 1 byte to memory
+
 	}
+
+	//while (!(MU_LSR & MU_LSR_TX_IDLE) && !(MU_LSR & MU_LSR_TX_EMPTY));
+	//MU_IO = (unsigned int)'A';
+
+	typedef int (*functype)(void);
+
+	functype f = (functype)ENTRY_ADDRESS;
+	f();
 
 	return 0;
 }
